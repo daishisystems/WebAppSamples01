8cdfed1b2703d4c515c2ade05609ae90eda09f7f
Paul Mooney <paul@gmst.ie>
2022-06-17 15:34:57 +0100

    Add project files.

---
+++ Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAppSamples01
+{
+    public class Global : HttpApplication
+    {
+        private void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RouteTable.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
